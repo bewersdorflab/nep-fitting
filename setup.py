c0104783818bc5a0282afdf889729a51deec7ff1
Andrew Barentine <andrew.barentine@yale.edu>
2017-09-28 15:22:27 -0400

    updating readme and reverting setup.py

--- setup.py
+++ setup.py
@@ -4,16 +4,9 @@
 from setuptools import setup, find_packages
 
 setup(name='nep_fitting',
-      version='1.2',
+      version='1.0',
       description='Nested Ensemble Resolution Estimation',
       author='Andrew Barentine, Michael Graff, David Baddeley',
       author_email='andrew.barentine@yale.edu',
       packages=find_packages(),
      )
-try:
-      import install_plugin
-      install_plugin.main()
-except:
-      import traceback
-      print('Restistering nep-fitting as a PYME plug-in may have failed:')
-      print(traceback.format_exception())
