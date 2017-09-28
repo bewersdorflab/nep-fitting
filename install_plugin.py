70a80fff1835ee672e9e4bbef0cc553a7512905f
Andrew Barentine <andrew.barentine@yale.edu>
2017-09-28 13:01:55 -0400

    setup script now automatically tries to install nep-fitting as a pyme plugin, to make the conda install complete

--- install_plugin.py
+++ install_plugin.py
@@ -2,6 +2,10 @@ from PYME import config
 import os
 from distutils.dir_util import copy_tree
 
-this_dir = os.path.dirname(__file__)
+def main():
+    this_dir = os.path.dirname(__file__)
 
-copy_tree(os.path.join(this_dir, 'etc', 'PYME'), config.user_config_dir)
+    copy_tree(os.path.join(this_dir, 'etc', 'PYME'), config.user_config_dir)
+
+if __name__ == '__main__':
+    main()
