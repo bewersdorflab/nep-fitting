f0a0ae2b30ce960f090956d7300f49eab9bc20a7
Andrew Barentine <31105780+barentine@users.noreply.github.com>
2017-09-28 15:51:44 -0400

    making install_plugin safe for being called without any arguments

--- nep_fitting/install_plugin.py
+++ nep_fitting/install_plugin.py
@@ -6,10 +6,11 @@ from distutils.dir_util import copy_tree
 def main():
     this_dir = os.path.dirname(__file__)
 
-    if sys.argv[1] == 'dist':
-        copy_tree(os.path.join(this_dir, 'etc', 'PYME'), config.dist_config_directory)
-    else:
-        copy_tree(os.path.join(this_dir, 'etc', 'PYME'), config.user_config_dir)
+    try:
+        if sys.argv[1] == 'dist':
+            copy_tree(os.path.join(this_dir, '..', 'etc', 'PYME'), config.dist_config_directory)
+    except IndexError:  # no argument provided, default to user config directory
+        copy_tree(os.path.join(this_dir, '..', 'etc', 'PYME'), config.user_config_dir)
 
 if __name__ == '__main__':
     main()
