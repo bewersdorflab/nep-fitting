a9e5c28991cbe9a770a7bd9a7c4345a45ca75ff1
Andrew Barentine <31105780+barentine@users.noreply.github.com>
2017-09-28 13:01:55 -0400

    setup script now automatically tries to install nep-fitting as a pyme plugin, to make the conda install complete

--- setup.py
+++ setup.py
@@ -4,10 +4,16 @@
 from setuptools import setup, find_packages
 
 setup(name='nep_fitting',
-      version='1.0',
+      version='1.2',
       description='Nested Ensemble Resolution Estimation',
       author='Andrew Barentine, Michael Graff, David Baddeley',
-      author_email='gward@python.net',
-      #url='https://www.python.org/sigs/distutils-sig/',
+      author_email='andrew.barentine@yale.edu',
       packages=find_packages(),
      )
+try:
+      import install_plugin
+      install_plugin.main()
+except:
+      import traceback
+      print('Restistering nep-fitting as a PYME plug-in may have failed:')
+      print(traceback.format_exception())
