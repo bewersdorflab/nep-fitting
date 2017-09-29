13e6e144cfb6c3714d0176fa0226eb07a54ba4e4
David Baddeley <david.baddeley.nz@gmail.com>
2017-09-29 14:32:49 +1300

    gui changes

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+#from distutils.core import setup
+from setuptools import setup, find_packages
+
+setup(name='nep_fitting',
+      version='1.2',
+      description='Nested Ensemble Resolution Estimation',
+      author='Andrew Barentine, Michael Graff, David Baddeley',
+      author_email='andrew.barentine@yale.edu',
+      packages=find_packages(),
+     )
