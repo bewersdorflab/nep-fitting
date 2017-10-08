16b4df853f002fcc554215a622247cbe73dcd67e
Andrew Barentine <31105780+barentine@users.noreply.github.com>
2017-10-08 13:18:34 -0400

    making single_data report safe for non-ensemble data, and fixing query to check for image schematic (apparently jinja counts NoneType as defined)

---
+++ nep_fitting/recipe_modules/nep_fits.py
@@ -0,0 +1,293 @@
+from PYME.recipes.base import register_module, ModuleBase, Filter
+from PYME.recipes.traits import Input, Output, Float, Enum, CStr, Bool, Int, List, DictStrStr, DictStrList, ListFloat, ListStr
+
+import numpy as np
+from PYME.IO import tabular
+from PYME.IO import MetaDataHandler
+
+from nep_fitting.core import profile_fitters, region_fitters
+from nep_fitting.core.handlers import LineProfileHandler, RegionHandler
+
+
+@register_module('TestEnsembleParameters')
+class TestEnsembleParameters(ModuleBase):
+    inputName = Input('profiles')
+
+    fit_type = CStr(profile_fitters.fitters.keys()[0])
+    ensemble_test_values = DictStrList({'psf_fwhm': [30, 35, 40, 45, 50, 55, 60, 65, 70, 75, 80]})
+
+    outputName = Output('fit_results')
+
+    def execute(self, namespace):
+
+
+        inp = namespace[self.inputName]
+
+        # generate LineProfileHandler from tables
+        handler = LineProfileHandler()
+        handler._load_profiles_from_list(inp)
+
+        fit_class = profile_fitters.ensemble_fitters[self.fit_type]
+        fitter = fit_class(handler)
+
+        results = fitter.ensemble_test(self.ensemble_test_values)
+        res = tabular.recArrayInput(results)
+
+        # propagate metadata, if present
+        res.mdh = MetaDataHandler.NestedClassMDHandler(getattr(inp, 'mdh', None))
+        
+        res.mdh['TestEnsembleParameters.FitType'] = self.fit_type
+        res.mdh['TestEnsembleParameters.EnsembleTestValues'] = self.ensemble_test_values
+
+        namespace[self.outputName] = res
+
+    @property
+    def _fitter_choices(self):
+        return profile_fitters.ensemble_fitters.keys()
+
+
+    @property
+    def default_view(self):
+        from traitsui.api import View, Item
+        from PYME.ui.custom_traits_editors import CBEditor
+
+        return View(Item('inputName', editor=CBEditor(choices=self._namespace_keys)),
+                    Item('_'),
+                    Item('fit_type', editor=CBEditor(choices=self._fitter_choices)),
+                    Item('_'),
+                    Item(''),
+                    Item('ensemble_test_values'),
+                    Item(''),
+                    Item('outputName'), buttons=['OK'])
+    
+    @property
+    def pipeline_view(self):
+        from traitsui.api import View, Item
+        from PYME.ui.custom_traits_editors import CBEditor
+    
+        return View(
+                    Item('fit_type', editor=CBEditor(choices=self._fitter_choices)),
+                    Item('_'),
+                    Item(''),
+                    Item('ensemble_test_values'),
+                    )
+
+@register_module('EnsembleFitProfiles')
+class EnsembleFitProfiles(ModuleBase):
+    inputName = Input('line_profiles')
+
+    fit_type = CStr(profile_fitters.ensemble_fitters.keys()[0])
+    ensemble_parameter_guess = Float(50.)
+    hold_ensemble_parameter_constant = Bool(False)
+
+    outputName = Output('fit_results')
+
+    def execute(self, namespace):
+
+
+        inp = namespace[self.inputName]
+
+        # generate LineProfileHandler from tables
+        handler = LineProfileHandler()
+        handler._load_profiles_from_list(inp)
+
+        fit_class = profile_fitters.ensemble_fitters[self.fit_type]
+        fitter = fit_class(handler)
+
+        if self.hold_ensemble_parameter_constant:
+            fitter.fit_profiles(self.ensemble_parameter_guess)
+        else:
+            fitter.ensemble_fit(self.ensemble_parameter_guess)
+
+        res = tabular.recArrayInput(fitter.results)
+
+        # propagate metadata, if present
+        res.mdh = MetaDataHandler.NestedClassMDHandler(getattr(inp, 'mdh', None))
+
+        res.mdh['EnsembleFitProfiles.FitType'] = self.fit_type
+        res.mdh['EnsembleFitProfiles.EnsembleParameterGuess'] = self.ensemble_parameter_guess
+        res.mdh['EnsembleFitProfiles.HoldEnsembleParamConstant'] = self.hold_ensemble_parameter_constant
+
+        namespace[self.outputName] = res
+
+    @property
+    def _fitter_choices(self):
+        return profile_fitters.ensemble_fitters.keys()
+
+
+    @property
+    def default_view(self):
+        from traitsui.api import View, Item
+        from PYME.ui.custom_traits_editors import CBEditor
+
+        return View(Item('inputName', editor=CBEditor(choices=self._namespace_keys)),
+                    Item('_'),
+                    Item('fit_type', editor=CBEditor(choices=self._fitter_choices)),
+                    Item('_'),
+                    Item('ensemble_parameter_guess'),
+                    Item('_'),
+                    Item('hold_ensemble_parameter_constant'),
+                    Item('_'),
+                    Item('outputName'), buttons=['OK'])
+
+    @property
+    def pipeline_view(self):
+        from traitsui.api import View, Item
+        from PYME.ui.custom_traits_editors import CBEditor
+    
+        return View(Item('fit_type', editor=CBEditor(choices=self._fitter_choices)),
+                    Item('_'),
+                    Item('ensemble_parameter_guess'),
+                    Item('_'),
+                    Item('hold_ensemble_parameter_constant'),
+                    )
+
+    @property
+    def dsview_view(self):
+        from traitsui.api import View, Item
+        from PYME.ui.custom_traits_editors import CBEditor
+
+        return View(Item('fit_type', editor=CBEditor(choices=self._fitter_choices)),
+                    Item('_'),
+                    Item('ensemble_parameter_guess'),
+                    Item('_'),
+                    Item('hold_ensemble_parameter_constant'), buttons=['OK'])
+
+
+@register_module('FitProfiles')
+class FitProfiles(ModuleBase):
+    inputName = Input('profiles')
+
+    fit_type = CStr(profile_fitters.fitters.keys()[0])
+
+    outputName = Output('fit_results')
+
+    def execute(self, namespace):
+
+
+        inp = namespace[self.inputName]
+
+        # generate LineProfileHandler from tables
+        handler = LineProfileHandler()
+        handler._load_profiles_from_list(inp)
+
+        fit_class = profile_fitters.fitters[self.fit_type]
+        fitter = fit_class(handler)
+
+        fitter.fit_profiles()
+
+        res = tabular.recArrayInput(fitter.results)
+
+        # propagate metadata, if present
+        res.mdh = MetaDataHandler.NestedClassMDHandler(getattr(inp, 'mdh', None))
+
+        res.mdh['FitProfiles.FitType'] = self.fit_type
+
+        namespace[self.outputName] = res
+
+    @property
+    def _fitter_choices(self):
+        return profile_fitters.fitters.keys()
+
+
+    @property
+    def default_view(self):
+        from traitsui.api import View, Item
+        from PYME.ui.custom_traits_editors import CBEditor
+
+        return View(Item('inputName', editor=CBEditor(choices=self._namespace_keys)),
+                    Item('_'),
+                    Item('fit_type', editor=CBEditor(choices=self._fitter_choices)),
+                    Item('_'),
+                    Item('outputName'), buttons=['OK'])
+
+    @property
+    def pipeline_view(self):
+        from traitsui.api import View, Item
+        from PYME.ui.custom_traits_editors import CBEditor
+    
+        return View(
+                    Item('fit_type', editor=CBEditor(choices=self._fitter_choices)),
+                    )
+
+@register_module('EnsembleFitROIs')
+class EnsembleFitROIs(ModuleBase):  # Note that this should probably be moved somewhere else
+    inputName = Input('ROIs')
+
+    fit_type = CStr('LorentzianConvolvedSolidSphere_ensemblePSF')
+    ensemble_parameter_guess = Float(50.)
+    hold_ensemble_parameter_constant = Bool(False)
+
+    outputName = Output('fit_results')
+
+    def execute(self, namespace):
+
+
+        inp = namespace[self.inputName]
+
+        # generate RegionHandler from tables
+        handler = RegionHandler()
+        handler._load_from_list(inp)
+
+        fit_class = region_fitters.fitters[self.fit_type]
+        fitter = fit_class(handler)
+
+        if self.hold_ensemble_parameter_constant:
+            fitter.fit_profiles(self.ensemble_parameter_guess)
+        else:
+            fitter.ensemble_fit(self.ensemble_parameter_guess)
+
+        res = tabular.recArrayInput(fitter.results)
+
+        # propagate metadata, if present
+        res.mdh = MetaDataHandler.NestedClassMDHandler(getattr(inp, 'mdh', None))
+
+        res.mdh['EnsembleFitROIs.FitType'] = self.fit_type
+        res.mdh['EnsembleFitROIs.EnsembleParameterGuess'] = self.ensemble_parameter_guess
+        res.mdh['EnsembleFitROIs.HoldEnsembleParamConstant'] = self.hold_ensemble_parameter_constant
+
+        namespace[self.outputName] = res
+
+    @property
+    def _fitter_choices(self):
+        return region_fitters.ensemble_fitters.keys() #FIXME???
+
+
+    @property
+    def default_view(self):
+        from traitsui.api import View, Item
+        from PYME.ui.custom_traits_editors import CBEditor
+
+        return View(Item('inputName', editor=CBEditor(choices=self._namespace_keys)),
+                    Item('_'),
+                    Item('fit_type', editor=CBEditor(choices=self._fitter_choices)),
+                    Item('_'),
+                    Item('ensemble_parameter_guess'),
+                    Item('_'),
+                    Item('hold_ensemble_parameter_constant'),
+                    Item('_'),
+                    Item('outputName'), buttons=['OK'])
+
+    @property
+    def pipeline_view(self):
+        from traitsui.api import View, Item
+        from PYME.ui.custom_traits_editors import CBEditor
+    
+        return View(
+                    Item('fit_type', editor=CBEditor(choices=self._fitter_choices)),
+                    Item('_'),
+                    Item('ensemble_parameter_guess'),
+                    Item('_'),
+                    Item('hold_ensemble_parameter_constant'),
+                    )
+
+    @property
+    def dsview_view(self):
+        from traitsui.api import View, Item
+        from PYME.ui.custom_traits_editors import CBEditor
+
+        return View(Item('fit_type', editor=CBEditor(choices=self._fitter_choices)),
+                    Item('_'),
+                    Item('ensemble_parameter_guess'),
+                    Item('_'),
+                    Item('hold_ensemble_parameter_constant'), buttons=['OK'])
---
+++ nep_fitting/reports/__init__.py
@@ -0,0 +1,41 @@
+import jinja2
+import os
+import base64
+
+env = jinja2.Environment(loader=jinja2.FileSystemLoader(os.path.split(__file__)[0]))
+
+from PYME.Analysis import graphing_filters #FIXME - move the filters somewhere better
+#env.filters['movieplot'] = graphing_filters.movieplot2
+env.filters['plot'] = graphing_filters.plot
+env.filters['hist'] = graphing_filters.hist
+
+#filter to encode base 64 encode data
+env.filters['b64encode'] = base64.b64encode
+
+schematic_files = {
+    'STEDTubule_Lumen': 'STED_lumen_labeled_tubule.svg',
+    # 'STEDTubuleMembrane': STEDTubuleMembrane,  # thin membrane approximation is depreciated
+    'STEDTubule_SurfaceAntibody': 'STED_tubule_antibody.svg',
+    'STEDTubule_SurfaceSNAP': 'STED_SNAP_labeled_tubule.svg',
+    'STEDMicrotubule_SurfaceAntibody': 'STED_antibody_microtubule.svg',
+}
+
+def generate(context, template_name):
+    template = env.get_template(template_name)
+    return template.render(**context)
+
+def generate_and_save(filename, context, template_name):
+    with open(filename, 'w') as f:
+        f.write(generate(context, template_name))
+        
+def get_schematic(fit_type):
+    import codecs
+    schem_filename = schematic_files.get(fit_type, None)
+    
+    if schem_filename:
+        with open(os.path.join(os.path.split(__file__)[0], schem_filename), mode='r') as f:
+            s = f.read()
+            
+        return s #.encode('utf-8')
+    else:
+        return None
