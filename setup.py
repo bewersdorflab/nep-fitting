3384d2f6b81e64ed1bde81328b9d88102aa8926a
Andrew Barentine <31105780+barentine@users.noreply.github.com>
2017-10-24 17:02:38 -0400

    incrementing version number

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@
 from setuptools import setup, find_packages
 
 setup(name='nep_fitting',
-      version='1.0',
+      version='1.3',
       description='Nested Ensemble Resolution Estimation',
       author='Andrew Barentine, Michael Graff, David Baddeley',
       author_email='andrew.barentine@yale.edu',
