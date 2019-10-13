4ce1e3712c9095b80889d62c7c56d4dd83188c07
barentine <andrew.barentine@gmail.com>
2019-10-13 23:42:48 +0200

    adding test for ensemble fitters

--- nep_fitting/tests/test_fitters.py
+++ nep_fitting/tests/test_fitters.py
@@ -1,6 +1,6 @@
 
 import numpy as np
-from nep_fitting.core import profile_fitters, handlers, rois, models
+from nep_fitting.core import profile_fitters, handlers, rois
 
 X = np.arange(100.)
 GROUND = {
@@ -27,7 +27,13 @@ def get_ground(fitter):
     """
     for d in fitter._fit_result_dtype:
         if d[0] == 'fitResults':
-            return [GROUND[field[0]] for field in d[1]]
+            ground = [GROUND[field[0]] for field in d[1]]
+        if d[0] == 'ensemble_parameter':
+            ens = [GROUND[field[0]] for field in d[1]]
+
+    if fitter._ensemble_parameter is None:
+        return ground
+    return ground, ens
 
 def test_non_ensemble_fitters():
     for fitter_class in profile_fitters.fitters.values():
@@ -38,4 +44,18 @@ def test_non_ensemble_fitters():
         h.add_line_profile(lp, update=False)
         fitter = fitter_class(h)
         fitter.fit_profiles()
-        np.testing.assert_allclose(fitter.results['fitResults'][0].tolist(), ground, rtol=10)
+        np.testing.assert_allclose(fitter.results['fitResults'][0].tolist(), ground, atol=5, rtol=0.15)
+
+def test_ensemble_fitters():
+    for fitter_class in profile_fitters.ensemble_fitters.values():
+        ground, ens = get_ground(fitter_class)
+        # leave out poisson sampling since we're only making one profile and with noise the lumen fitters mix psf / diam
+        p = fitter_class._model_function(None, ground, X, ens)
+        lp = rois.LineProfile(distance=X, profile=p)
+        h = handlers.LineProfileHandler()
+        h.add_line_profile(lp, update=False)
+        fitter = fitter_class(h)
+        fitter.ensemble_fit(ens)
+        np.testing.assert_allclose(fitter.results['fitResults'][0].tolist(), ground, rtol=0.1)
+        np.testing.assert_allclose(fitter.results['ensemble_parameter'][0].tolist(), ens, rtol=0.1)
+
