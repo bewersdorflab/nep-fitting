7904baf010c31f4b0b3a5d6a636e73908bbf3317
barentine <andrew.barentine@gmail.com>
2019-11-12 12:17:54 -0500

    adding test for new fitters

--- nep_fitting/tests/test_fitters.py
+++ nep_fitting/tests/test_fitters.py
@@ -38,9 +38,10 @@ def get_ground(fitter):
     return ground, ens
 
 def test_non_ensemble_fitters():
-    for fitter_class in profile_fitters.fitters.values():
+    for fitter_class in profile_fitters.non_ensemble_fitters.values():
         ground = get_ground(fitter_class)
-        p = np.random.poisson(fitter_class._model_function(None, ground, X))
+        # leave out noise for non-ensemble fitters, otherwise results will be poor when fitting a single profile
+        p = fitter_class._model_function(ground, X)
         lp = rois.LineProfile(distance=X, profile=p)
         h = handlers.LineProfileHandler()
         h.add_line_profile(lp, update=False)
@@ -48,11 +49,20 @@ def test_non_ensemble_fitters():
         fitter.fit_profiles()
         np.testing.assert_allclose(fitter.results['fitResults'][0].tolist(), ground, atol=5, rtol=0.15)
 
+
+def test_naive_fitter_single_fits():
+    for fitter_class in profile_fitters.naive_fitters.values():
+        ground = get_ground(fitter_class)
+        p = np.random.poisson(fitter_class._model_function(ground, X))
+        lp = rois.LineProfile(distance=X, profile=p)
+        results = fitter_class.fit_single_profile(lp)
+        np.testing.assert_allclose(results['fitResults'][0].tolist(), ground, atol=5, rtol=0.15)
+
 def test_ensemble_fitters():
     for fitter_class in profile_fitters.ensemble_fitters.values():
         ground, ens = get_ground(fitter_class)
         # leave out poisson sampling since we're only making one profile and with noise the lumen fitters mix psf / diam
-        p = fitter_class._model_function(None, ground, X, ens)
+        p = fitter_class._model_function(ground, X, ens)
         lp = rois.LineProfile(distance=X, profile=p)
         h = handlers.LineProfileHandler()
         h.add_line_profile(lp, update=False)
@@ -60,4 +70,3 @@ def test_ensemble_fitters():
         fitter.ensemble_fit(ens)
         np.testing.assert_allclose(fitter.results['fitResults'][0].tolist(), ground, rtol=0.1)
         np.testing.assert_allclose(fitter.results['ensemble_parameter'][0].tolist(), ens, rtol=0.1)
-
